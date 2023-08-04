0601eb1bac36e6fb362d59779f6b81db7eb6b0da
kirankamal <kiran.k@lucidplus.com>
2023-08-04 17:04:42 +0530

    fix : navigation issues

--- lib/features/login/presentation/login_page.dart
+++ lib/features/login/presentation/login_page.dart
@@ -188,7 +188,7 @@ class _LoginPageState extends State<LoginPage> {
                             ),
                             TextButton(
                               onPressed: () {
-                                Navigator.pushReplacement(
+                                Navigator.push(
                                   context,
                                   MaterialPageRoute(
                                     builder: (context) => const RegisterPage(),
